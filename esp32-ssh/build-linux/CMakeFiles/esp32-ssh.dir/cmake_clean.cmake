02e8794de8d6b3dddea27fa57f985072b7df9e07
OpenNoorIlm <noorulilm.contact.us@gmail.com>
2026-09-03 15:20:40 +0530

    Added some updates

---
+++ esp32-ssh/build-linux/CMakeFiles/esp32-ssh.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/esp32-ssh.dir/link.d"
+  "CMakeFiles/esp32-ssh.dir/src/main.cpp.o"
+  "CMakeFiles/esp32-ssh.dir/src/main.cpp.o.d"
+  "CMakeFiles/esp32-ssh.dir/src/sha256.cpp.o"
+  "CMakeFiles/esp32-ssh.dir/src/sha256.cpp.o.d"
+  "esp32-ssh"
+  "esp32-ssh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/esp32-ssh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
